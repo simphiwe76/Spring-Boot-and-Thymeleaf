cc8cb16466ee88b53725573448faaa2dc3c6fcad
Simphiwe Mthanti <simphiwe@dots.africa>
2023-04-06 12:39:40 +0200

    website

--- src/main/java/phakamani/creche/website/controller/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package phakamani.creche.website.controller;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException() {
-        super();
-    }
-}
