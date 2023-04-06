4376e93b9f1b086b0d6d1cdd066f0ced957c5629
Simphiwe Mthanti <simphiwe@dots.africa>
2023-04-06 11:54:42 +0200

    website

--- src/main/java/phakamani/creche/website/controller/GlobalExceptionHandler.java
+++ src/main/java/phakamani/creche/website/controller/GlobalExceptionHandler.java
@@ -4,13 +4,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler({ ResourceNotFoundException.class })
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleNotFoundException(Exception ex) {
-        return "index";
+    public ModelAndView handleNotFoundException(Exception ex) {
+        return new ModelAndView("index");
     }
 }
